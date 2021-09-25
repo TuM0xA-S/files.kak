34c6941585a97bc24c1aa6fbec07471deae68151
Timofey Safronov <tsafronov@ozon.ru>
2021-09-25 16:53:22 +0300

    optimize long format

--- nice-ls.sh
+++ nice-ls.sh
@@ -25,16 +25,16 @@ fi
 
 counter=0
 gutter="$(mktemp)"
-paste -d '\n' <($lscmd -Ll "$1" | sed "1d") <($lscmd "$1" | grep -o '.$') | while read a && read b; do
-    ((counter++))
+paste -d '\n' <($lscmd -L "$1") <($lscmd "$1" | grep -o '.$') <($lscmd -Ll "$1" | sed "1d" | grep -Po "(\S+\s+){7}") | while read a && read b && read c; do
     output="$a"
-    info="$(echo "$output" | grep -Po "(\S+\s+){7}")"
-    echo -n "'$counter|$info' " >> "$gutter"
-    output="$(echo "$output" | grep -Po "(?<=$info).*")"
     if [ "$b" = "@" ]; then
         output="$output$b"
     fi
     echo "$output"
+
+    ((counter++))
+    info="$c"
+    echo -n "'$counter|$info ' " >> "$gutter"
 done
 
 cat "$gutter"
